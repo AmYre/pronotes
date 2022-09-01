35581ccabe581bc663849312d463a0296c2903cf
amyre <benhammouda.amir@gmail.com>
2022-09-01 15:30:28 +0200

    cleaning getters and setters

--- src/main/java/com/SimplonOcto/API/model/Rattrapage.java
+++ src/main/java/com/SimplonOcto/API/model/Rattrapage.java
@@ -23,24 +23,4 @@ public class Rattrapage {
 	
 	private String pdf_sujet;
 	
-	public Rattrapage() {
-		
-	}
-	
-	public Rattrapage(Integer id_rattrapage, Date date_rattrapage, String libelle, String pdf_sujet) {
-		this.id_rattrapage = id_rattrapage;
-		this.date_rattrapage = date_rattrapage;
-		this.libelle = libelle;
-		this.pdf_sujet = pdf_sujet;
-	}
-	
-	public Integer getId_rattrapage () {return id_rattrapage;}
-	public Date getDate_rattrapage () {return date_rattrapage;}
-	public String libelle () {return libelle;}
-	public String pdf_sujet () {return pdf_sujet;}
-	public Integer setId_rattrapage (Integer id_rattrapage) { return this.id_rattrapage = id_rattrapage; }
-	public Date setDate_rattrapagee (Date date_rattrapage) { return this.date_rattrapage = date_rattrapage; }
-	public String setLibelle (String libelle) { return this.libelle = libelle; }
-	public String setPdf_sujet (String pdf_sujet) { return this.pdf_sujet = pdf_sujet; }
-	
 }
--- src/main/java/com/SimplonOcto/API/model/Surveillant.java
+++ src/main/java/com/SimplonOcto/API/model/Surveillant.java
@@ -1,8 +1,5 @@
 package com.SimplonOcto.API.model;
 
-import java.sql.Date;
-
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
@@ -24,25 +21,5 @@ public class Surveillant {
 	private String email;
 	
 	private String password;
-	
-	public Surveillant() {
-		
-	}
-	
-	public Surveillant(Integer id_surveillant, String prenom, String nom, String password) {
-		this.id_surveillant = id_surveillant;
-		this.prenom = prenom;
-		this.nom = nom;
-		this.password = password;
-	}
-	
-	public Integer getId_surveillant () {return id_surveillant;}
-	public String getPrenom () {return prenom;}
-	public String getNom () {return nom;}
-	public String getPassword () {return password;}
-	public Integer setId_surveillant (Integer id_surveillant) { return this.id_surveillant = id_surveillant; }
-	public String setPrenom (String prenom) { return this.prenom = prenom; }
-	public String setNom (String nom) { return this.nom = nom; }
-	public String setPassword (String password) { return this.password = password; }
 	
 }
